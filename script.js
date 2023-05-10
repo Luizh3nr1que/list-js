d829365cb10179d8dfa67185da3b05ef1ed3e1d3
luizh3nrique <luizh3nrique00@gmail.com>
2023-05-10 13:32:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+let docTitle = document.title;
+window.addEventListener('blur', () => {
+    document.title = 'Veja a sua lista'
+});
+window.addEventListener('focus', () => {
+    document.title = docTitle;
+});
+
+
+
+const lista = document.getElementById('lista');
+const ul = document.querySelector('ul');
+const listaTarefa = document.getElementById('lista-tarefa');
+const erro = document.querySelector('.erro');
+
+
+
+lista.addEventListener('keyup', handleKeyUp);
+
+function handleKeyUp(e) {
+    if (e.key === "Enter") {
+        if (lista.value === "") {
+            erro.innerHTML = 'Digite algo na lista para adicionar';
+        } else {
+            erro.innerHTML = '';
+            let novoItem = document.createElement("li");
+            novoItem.innerHTML = lista.value;
+
+            let checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.classList.add("checkbox");
+            novoItem.appendChild(checkbox);
+
+
+            checkbox.addEventListener('change', function () {
+                if (this.checked) {
+                    novoItem.style.color = 'red';
+                    novoItem.style.textDecoration = 'line-through';
+                    novoItem.style.animation = 'riscar 1s linear forwards';
+                } else {
+                    novoItem.style.color = '#c9c9c9';
+                    novoItem.style.textDecoration = 'none';
+                    novoItem.style.animation = 'none';
+                }
+            });
+
+
+
+            ul.appendChild(novoItem);
+            lista.value = "";
+        }
+    }
+}
+
+
+function limpar() {
+    listaTarefa.innerHTML = ' ';
+    lista.value = "";
+
+}
+
+
